03d3fd9642435ec52db43907acb999ec518c6a76
Louis Van Steene <louisvansteene@gmail.com>
2021-04-02 17:43:00 +0100

    Bootstrapify Devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  validates :terms_of_service, acceptance: true
 end
--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,9 @@ module DiabetesMonitor
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    ActionView::Base.field_error_proc = proc do |html_tag, _instance|
+      html_tag.gsub('form-control', 'form-control is-invalid').html_safe # rubocop:disable Rails/OutputSafety
+    end
   end
 end
