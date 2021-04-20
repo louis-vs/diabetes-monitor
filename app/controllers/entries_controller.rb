e3b9d1f9eb0d3b0995db502ddd608964d7f84fe5
Louis Van Steene <louisvansteene@gmail.com>
2021-04-20 12:46:57 +0100

    Add translations

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -48,7 +48,7 @@ class EntriesController < ApplicationController
   def update
     respond_to do |format|
       if @entry.update(entry_params)
-        format.html { redirect_to entries_path, notice: 'Entry was successfully updated.' }
+        format.html { redirect_to entries_path, notice: t('.success') }
       else
         format.html { render :edit }
       end
