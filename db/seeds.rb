9a7588d2855583e921325b953c7f19963a93805b
Louis Van Steene <l+gh@lvs.me.uk>
2021-06-17 19:02:51 +0100

    Fix linter error

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -9,4 +10,3 @@
 
 user = User.create(email: 'someone@someaddress.com', name: 'Steve', password: 'password')
 user.confirm
-
